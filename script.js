6caf3c0bdc428964006d75a0553b617ad7c0d79a
Lukhanyo Kalashe <kalashslukhanyo@gmail.com>
2024-10-17 15:06:26 +0200

    With some Javascript and lessons

--- script.js
+++ script.js
@@ -1,30 +1,69 @@
-const myFunc = (otherFunc) => {
-    otherFunc();
+
+
+let count = 0;
+setInterval(() => {
+    console.log(count);
+    // count++;
+}, 1);
+
+
+
+const url = 'https://restcountries.com/v3.1/all';
+
+
+const GetSA = (arr) => {
+    arr.find(e => e.cca2 === "ZA")
+    const ans = arr.filter(e => e.region === "Africa")
+    console.log(ans)
+}
+// const findSA = (arr) => {
+//     // for(let index = 0; index < arr.length; index++)
+//     // {
+//     //     const element = arr[index];
+//     //     if(element.cca2 ==="ZA")
+//     //         return element;
+    
+//     // }
+
+//     let ans = arr.find(e => e.cca2 === "ZA");
+//     console.log(ans)
+// }
+const changeJogn = (data) => {
+    const SouthAH = GetSA(data);
+    console.log(SouthAH);
+    document.getElementById("profilePhoto").src = SouthAH.flags.png
+    
+}
+const data = fetch(url).then((respnse)=> {
+    console.log(respnse);
+    respnse.json().then(data =>{
+        console.log(data);
+        changeJogn(data)
+    })
+})
+console.log(data)
+
+//thenable stuff
+//fetch it then .,...
+
+
+
+
+const payload = {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": true
 }
 
-myFunc(() => {
-    console.log("Im the other Function");
-});
+
+const requestOptions = {
+method: 'POST',
+headers: { 'Content-Type': 'application/json' },
+body: JSON.stringify({ title: 'Fetch POST Request Example' })
+};
 
 
-
-let title = document.getElementById("title")
-
-
-
-
-let OldTitle = title.innerText;
-let newTitle = "BBD is great"
-
-let image = document.getElementById("profilePhoto");
-console.log(image);
-let body = document.getElementsByTagName("body")[0];
-console.log(body);
-
-let OtherImage = image.cloneNode(true)
-function  SayHello()
-{
-    // let TempImage = image
-   
-    body.appendChild(OtherImage);
+const send = (task) => {
+    fetch()
 }
